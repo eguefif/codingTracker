f96420452b6d49844f0ffedab3793f2609497fe1
Emmanuel Guefif <eguefif@fastmail.com>
2023-08-12 07:22:07 -0400

    Created test with pytest for VimProcess class

--- codingTracker/client.py
+++ codingTracker/client.py
@@ -14,15 +14,15 @@ LANGUAGES = {"py": "python",
 class VimProcess:
     def __init__(self, ps_entry: str):
         self.ps_entry: str = ps_entry
-        self.pid: int = self.get_pid()
+        self.pid: int = int(self.get_pid())
         self.language: str = self.get_language().strip()
         self.start_time: struct_time = self.get_start_time()
 
     def get_pid(self) -> int:
-        return re.search(r"\b[0-9]+", self.ps_entry).group()
+        return re.search(r"\b[0-9]+\b", self.ps_entry).group()
 
     def get_language(self) -> str:
-        vim_file = re.search(r"(?<=vim)\s*[a-zA-Z0-9.]+", self.ps_entry).group()
+        vim_file = re.search(r"(?<=vim)\s*[a-zA-Z0-9.,-_() ]+", self.ps_entry).group()
         extension = self.get_extension(vim_file)
         return LANGUAGES[extension]
 
@@ -34,7 +34,7 @@ class VimProcess:
             return None
 
     def get_start_time(self):
-        start_time = re.search(r"\b[0-9][0-9]:[0-9][0-9]\b", self.ps_entry).group()
+        start_time = re.search(r"\b[0-2][0-9]:[0-9][0-9]\b", self.ps_entry).group()
         s_time = struct_time((0, 0, 0, int(start_time[0:2]), int(start_time[3:]), 0, 0, None, -1))
         return s_time
 
