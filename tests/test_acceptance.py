87d452e6e4acb4e67856983e8f32a7e33099959e
Emmanuel Guefif <eguefif@fastmail.com>
2023-08-28 10:55:54 +0200

    Created file test and updated makefile

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -1,8 +1,18 @@
+import json
 import pytest
+import multiprocessing
+import subprocess
+from time import time, strftime
 
 from codingTracker.client import App as clientApp
 from codingTracker.server import App as serverApp
 
 
 def test_acceptance_always_connected() -> None:
-    assert 0
+    start_time: float = time()
+    end_time: float = time()
+    today: str = strftime("%j %y")
+    test_dict: dict[str, dict[str, list[float]]] = {today: {"python": [round(start_time), round(end_time)]}}
+    with open("./data_server.dat", "r") as f:
+        content: dict[str, dict[str, list[float]]] = json.load(f)
+    assert content == test_dict
